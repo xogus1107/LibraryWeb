a04484bb4a76de540fd95503f57683d67c61779d
dispe1 <lkjim0757@naver.com>
2018-12-15 01:53:43 +0900

    add return book

--- php/returnBook.php
+++ php/returnBook.php
@@ -2,19 +2,17 @@
     include "include/dbConnect.php";
     $id = $_POST['id'];
     $serial_number = $_POST['number'];
-    $today = date("Ymd");
 
 
-
-    $sql = "INSERT into library.rental values('{$id}','{$serial_number}','{$today}')";
+    $sql = "DELETE from library.rental where id = '{$id}'";
     if($dbConnect->query($sql)){
       $sql2 = "SELECT * FROM library.book WHERE serial_number = '{$serial_number}'";
       $res = $dbConnect->query($sql2);
       $row = $res->fetch_array(MYSQLI_ASSOC);
 
       if ($row != null) {
-        $rented_book_count = $row['rented_book_count'] + 1;
-        $sql3 = "UPDATE library.book set rented_book_count='{$rented_book_count}' where serial_number = '{$serial_number}'";
+        $rented_book_count = $row['rented_book_count'] - 1;
+        $sql3 = "UPDATE library.book set rented_book_count = '{$rented_book_count}' where serial_number = '{$serial_number}'";
         if($dbConnect->query($sql3)){
           echo json_encode(array('res'=>'good'));
         }else{
