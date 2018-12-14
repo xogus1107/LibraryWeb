fe8d2c8f2084c54587fc3cd6ba21106162208da0
Manny <yjm9554@gmail.com>
2018-12-14 16:33:13 +0900

    read book data from DB completed

--- js/findBookFromDB.js
+++ js/findBookFromDB.js
@@ -7,17 +7,62 @@ function search_book(){
         async: 'false',
         data: queryString,
         success:function(data){
-            $('#abcde').text(data);
+            //$('#abcde').text(data);
             parse_result = JSON.parse(data);
             console.log(parse_result);
 
-            for (var objVarJson in parse_result){
-            	console.log(parse_result[objVarJson]['title']);
-            }
+            for (var objJson in parse_result){
+                if(objJson == '6')
+                    break;
 
-           // $('#abcde').text(jsonobj);
+               var text = getTarget(objJson);
+               console.log(text);
+               if (text == 'null')
+                return;
+               else{
+                $(text+'_book_title').text(parse_result[objJson]['title']);
+                $(text+'_book_writer').text(parse_result[objJson]['writer']);
+                $(text+'_book_publisher').text(parse_result[objJson]['publisher']);
+                $(text+'_book_total').text(parse_result[objJson]['book_count']);
+                $(text+'_book_rented').text(parse_result[objJson]['rented_book_count']);
+               }
+
+            }
             
         }
     });
     return false;
 }
+
+
+function getTarget(num){
+    console.log(num);
+    var text;
+    switch (num){
+        case '0': text = "#first";
+        break;
+
+        case '1': text = '#second';
+        break;
+
+        case '2': text = '#third';
+        break;
+
+        case '3': text = '#fourth';
+        break;
+
+        case '4': text = '#fifth';
+        break;
+
+        case '5': text = '#sixth';
+        break;
+
+        default: text = 'null';
+        break;
+    }
+
+    console.log("abcde : "+text);
+
+    return text;
+}
+
